d2f65c9e16d53b4aab12f851979a9fcb330b5e3e
Josiah Maina <josiah5115@gmail.com>
2018-06-29 16:27:03 -0600

    react-afternoon-project

---
+++ src/components/Topics/EvenAndOdd.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+class EvenAndOdd extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      evenArray: [],
+      oddArray: [],
+      userInput: ''
+    }
+  }
+
+  handleChange(val) {
+    this.setState({ userInput: val });
+  }
+
+  render() {
+    return (
+    <div className="puzzleBox evenAndOddPB">
+      <h4> Evens and Odds </h4>
+      <input className="inputLine" onChange={ (e) => this.handleChange(e.target.value) }></input>
+      <button className="confirmationButton" onClick={ () => { this.assignEvenAndOdds(this.state.userInput) }}> Split </button>
+      <span className="resultsBox"> Evens: { JSON.stringify(this.state.evenArray) } </span>
+      <span className="resultsBox"> Odds: { JSON.stringify(this.state.oddArray) } </span>
+    </div>
+  )}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default EvenAndOdd;
